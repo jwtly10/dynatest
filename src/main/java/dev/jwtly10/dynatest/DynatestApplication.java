121b3b8fc416fe4ba53abbfd0c154614832b33de
jwtly10 <39057715+jwtly10@users.noreply.github.com>
2024-05-28 23:41:06 +0100

    new project setup

---
+++ src/main/java/dev/jwtly10/dynatest/DynatestApplication.java
@@ -0,0 +1,13 @@
+package dev.jwtly10.dynatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynatestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynatestApplication.class, args);
+    }
+
+}
